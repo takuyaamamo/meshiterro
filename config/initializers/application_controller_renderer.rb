995178c9a14c7a6cc08af53363fe7e302aaed7d1
Ama <Ama@amacbook.local>
2019-04-13 17:45:04 +0900

    [finish]5-3 on macbook

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '331c2b634ea2a1cdba6e860977732274d30af78ea838e4a376b5f1a49aea9f5ef5ea09c510ce4d65bfd80ea20fb83a1aa4090bf3ff1fabab800bc0f14fe61e1c'
