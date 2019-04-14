fbb90894f9f46cf6550bfa6a6d6278d897409200
ama <takuyaamano@amamo.local>
2019-04-14 14:50:07 +0900

    [update]rails db:migrate on favoriteModel

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@ class User < ApplicationRecord
 
   has_many :post_images, dependent: :destroy#PostImageモデルと1:Nの関係
   has_many :post_comments, dependent: :destroy#PostCommentモデルと1:Nの関係
+  has_many :favorites, dependent: :destroy#favoriteモデルと1:Nの関係
 
 end
